f1df9b171e9d7fe0310b496272c2dc5b685f5ffc
Elvira <eliasufa@yahoo.com>
2022-08-19 04:36:03 +0500

    4_2

---
+++ Task 4_2/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 44;
+int b = 5;
+int c = 78;
+int max = a;
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    Console.Write($"max = {c}");
+}
+else
+{
+    Console.Write($"max = {max}");
+}
