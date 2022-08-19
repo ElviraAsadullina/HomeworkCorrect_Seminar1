1569910efe6d2b15ff7448fe62daf4b6902f0dc8
Elvira <eliasufa@yahoo.com>
2022-08-19 18:07:54 +0500

    Удалены ненужные папки

--- Task 6_3/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿int a = 7;
-if (a % 2 == 0)
-{
-    Console.Write("Да");
-}
-else
-{
-    Console.Write("Нет");
-}
--- Task 8_2/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿int N = 8;
-int start = 2;
-while (start <= N)
-{
-    Console.Write(start);
-    Console.Write(" ");
-    start = start + 2;
-}
